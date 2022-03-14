2ac3300ab84c57d10012ae7f3b0208dcff511cd7
najro <orjan@najro.com>
2022-03-14 20:07:12 +0100

    added initial version with sample data

---
+++ PreviewToolForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace WindowsFormsPreviewTool
+{
+    partial class PreviewToolForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.webView2 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.AllowColumnReorder = true;
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(209, 694);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // webView2
+            // 
+            this.webView2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView2.BackColor = System.Drawing.Color.Black;
+            this.webView2.CreationProperties = null;
+            this.webView2.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView2.Location = new System.Drawing.Point(242, 12);
+            this.webView2.Name = "webView2";
+            this.webView2.Size = new System.Drawing.Size(933, 803);
+            this.webView2.Source = new System.Uri("https://www.microsoft.com", System.UriKind.Absolute);
+            this.webView2.TabIndex = 2;
+            this.webView2.UseWaitCursor = true;
+            this.webView2.ZoomFactor = 1D;
+            // 
+            // PreviewToolForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 820);
+            this.Controls.Add(this.webView2);
+            this.Controls.Add(this.listView1);
+            this.Name = "PreviewToolForm";
+            this.Text = "Preview tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView2;
+    }
+}
+
---
+++ PreviewToolForm.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsPreviewTool
+{
+    public partial class PreviewToolForm : Form
+    {
+
+        private const string FilesFolder = "files";
+        private const string PreviewFolder = "preview";
+        public PreviewToolForm()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            webView2.CoreWebView2InitializationCompleted += WebView_CoreWebView2InitializationCompleted;
+
+            InitializeListViewAndLoadFiles();
+
+            Debug.WriteLine("before InitializeAsync");
+            await InitializeAsync();
+            Debug.WriteLine("after InitializeAsync");
+
+            //webView2.CoreWebView2.Navigate("https://www.microsoft.com");
+            //Debug.WriteLine("after Navigate");
+
+            if ((webView2 == null) || (webView2.CoreWebView2 == null))
+            {
+                Debug.WriteLine("webview not ready");
+            }
+
+            webView2.Source = new Uri(Directory.GetCurrentDirectory() + $"\\{PreviewFolder}\\template\\start.htm");
+        }
+
+        private async Task InitializeAsync()
+        {
+            Debug.WriteLine("InitializeAsync");
+            await webView2.EnsureCoreWebView2Async(null);
+            Debug.WriteLine("WebView2 Runtime version: " + webView2.CoreWebView2.Environment.BrowserVersionString);
+        }
+
+        private void InitializeListViewAndLoadFiles()
+        {
+
+            listView1.FullRowSelect = true;
+            listView1.GridLines = true;
+            listView1.View = View.List;
+
+            var files = Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\{FilesFolder}", "*.*", SearchOption.AllDirectories);
+
+            var fileNames = new List<string>();
+
+            foreach (string file in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(file);
+
+                if (fileNames.Contains(fileName))
+                    continue;
+
+                fileNames.Add(fileName);
+                var item = new ListViewItem(fileName)
+                {
+                    Tag = file
+                };
+                listView1.Items.Add(item);
+            }
+
+
+            DirectoryInfo di = new DirectoryInfo($"{Directory.GetCurrentDirectory()}\\{PreviewFolder}");
+            foreach (FileInfo file in di.EnumerateFiles())
+            {
+                file.Delete();
+            }
+        }
+
+        private void WebView_CoreWebView2InitializationCompleted(object sender, Microsoft.Web.WebView2.Core.CoreWebView2InitializationCompletedEventArgs e)
+        {
+            Debug.WriteLine("WebView_CoreWebView2InitializationCompleted");
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                String fileName = listView1.SelectedItems[0].Text;
+                Debug.WriteLine(fileName + " is selected");
+
+                var currentFileDiretory = $"{Directory.GetCurrentDirectory()}\\{FilesFolder}";
+
+
+                // read json file
+                var jsonFile = $"{currentFileDiretory}\\{fileName}.json";
+                var jsonFileContent = "";
+                
+                if (File.Exists(jsonFile))
+                {
+                    jsonFileContent = string.Join("", File.ReadAllLines(jsonFile, Encoding.UTF8));
+                }
+
+                // read svg file
+                var svgFile = $"{currentFileDiretory}\\{fileName}.svg";
+
+                if (!File.Exists(svgFile))
+                {
+                    svgFile = string.Empty;
+                }
+
+                //// read jpg file
+                var jpgFile = $"{currentFileDiretory}\\{fileName}.jpg";
+
+                if (!File.Exists(jpgFile))
+                {
+                    jpgFile = string.Empty;
+                }
+
+
+                //// read png file
+                var pngFile = $"{currentFileDiretory}\\{fileName}.png";
+
+                if (!File.Exists(pngFile))
+                {
+                    pngFile = string.Empty;
+                }
+
+
+                BuildPreviewView(jsonFileContent, svgFile, jpgFile, pngFile, fileName);
+
+            }
+
+
+        }
+        private void BuildPreviewView(string jsonData, string svgUrl, string jpgUrl, string pngFile, string fileName)
+        {
+
+            
+
+
+            //var files = Directory.GetFiles($"{Directory.GetCurrentDirectory()}\\{FilesFolder}", "*.htm", SearchOption.AllDirectories);
+            //var fileNames = new List<string>();
+
+            //foreach (string file in files)
+            //{
+            //    var fileName = Path.gef(file);
+
+            //    if (fileNames.Contains(fileName))
+            //        continue;
+
+            //    fileNames.Add(fileName);
+            //    var item = new ListViewItem(fileName)
+            //    {
+            //        Tag = file
+            //    };
+            //    listView1.Items.Add(item);
+            //}
+
+            var templateFilePath = Directory.GetCurrentDirectory() + $"\\{PreviewFolder}\\template\\view-template.htm";
+            var previewHtml = File.ReadAllText(templateFilePath, Encoding.UTF8);
+
+            previewHtml = previewHtml.Replace("#chartJson#", !string.IsNullOrEmpty(jsonData) ? jsonData : "undefined");
+            previewHtml = previewHtml.Replace("#jpgFile#", !string.IsNullOrEmpty(jpgUrl) ? $"\"{jpgUrl.Replace("\\", "\\\\")}\"" : "undefined");
+            previewHtml = previewHtml.Replace("#svgFile#", !string.IsNullOrEmpty(svgUrl) ? $"\"{svgUrl.Replace("\\", "\\\\")}\"" : "undefined");
+            previewHtml = previewHtml.Replace("#pngFile#", !string.IsNullOrEmpty(pngFile) ? $"\"{svgUrl.Replace("\\", "\\\\")}\"" : "undefined");
+
+
+            var viewPath = Directory.GetCurrentDirectory() + $"\\preview\\{fileName}.htm";
+            File.WriteAllText(viewPath, previewHtml, Encoding.UTF8);
+
+            webView2.Source = new Uri(viewPath);
+        }
+    }
+}
